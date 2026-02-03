a2c9538ae446877b82ca774f6c71e1439f99b973
Stan Egorov <segorov@paloaltonetworks.com>
2026-02-03 09:21:19 -0800

    Remove WateringRule from rules __init__ exports

--- app/rules/__init__.py
+++ app/rules/__init__.py
@@ -1,10 +1,9 @@
 """Rule-based task generation engine"""
 from app.rules.task_generator import TaskGenerator
-from app.rules.rules import HarvestRule, WateringRule, SeedViabilityRule
+from app.rules.rules import HarvestRule, SeedViabilityRule
 
 __all__ = [
     "TaskGenerator",
     "HarvestRule",
-    "WateringRule",
     "SeedViabilityRule",
 ]
